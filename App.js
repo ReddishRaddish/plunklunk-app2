de11219aa050398d99746ab9e1ea7885e951c37a
Shreyaa <shreyaa555@gmail.com>
2018-09-09 15:54:58 -0400

    fixed react navigation

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {createStackNavigator} from 'react-navigation';
+import {StackNavigator} from 'react-navigation';
 import home from './Screens/home';
 
 export default class App extends React.Component {
@@ -12,7 +12,7 @@ export default class App extends React.Component {
   }
 } 
 
-const AppStackNavigator = createStackNavigator({
+const AppStackNavigator = StackNavigator({
   home: {screen: home},
 });
 
